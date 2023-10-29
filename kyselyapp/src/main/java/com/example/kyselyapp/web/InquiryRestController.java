689f3a5b3ca026b6e57a97240b30d9946b5fba38
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-10-29 23:29:50 +0200

    InquiryRestControllleria muokattu

--- kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryRestController.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryRestController.java
@@ -1,1 +1,33 @@
 package com.example.kyselyapp.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.kyselyapp.domain.Inquiry;
+import com.example.kyselyapp.domain.Inquiryrepository;
+
+@Controller
+public class InquiryRestController{
+	@Autowired
+	private Inquiryrepository repository;
+	
+
+
+@RequestMapping(value="/inquirys", method = RequestMethod.GET)
+public @ResponseBody List<Inquiry> inquiryListRest(){
+	return (List<Inquiry>) repository.findAll();
+}
+
+@RequestMapping(value="/inquiry/{id}", method = RequestMethod.GET)
+public @ResponseBody Optional<Inquiry> findInquiryRest(@PathVariable("id") Long inquiryid){
+	return repository.findById(inquiryid);
+
+	}
+}
