7879ea62a8ecf7dc8668733a8f342f97e62f5db4
Jenna <143401323+jenvii@user.noreply.github.com>
2023-10-30 11:46:33 +0200

    QuestionController.java, QuestionRepository.java, addquestion.html muokkauksia

---
+++ kyselyapp/src/main/java/com/example/kyselyapp/domain/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.example.kyselyapp.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionRepository extends CrudRepository<Question, Long> {
+	
+	List<Question> findByText(String text);
+}
--- kyselyapp/src/main/java/com/example/kyselyapp/web/QuestionController.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/web/QuestionController.java
@@ -1,1 +1,36 @@
 package com.example.kyselyapp.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.kyselyapp.domain.Question;
+import com.example.kyselyapp.domain.QuestionRepository;
+
+@Controller
+public class QuestionController {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@RequestMapping(value = "/allquestions", method = RequestMethod.GET)
+	public String questionList(Model model) {
+		model.addAttribute("questions", questionRepository.findAll());
+		return "questionlist";
+	}
+
+	@RequestMapping(value = "/addquestion")
+	public String addQuestion(Model model) {
+		model.addAttribute("question", new Question());
+		return "addquestion";
+	}
+
+	@RequestMapping(value="/save")
+	public String saveQuestion(Question question) {
+		questionRepository.save(question);
+		return "redirect:allquestions";
+	}
+
+}
