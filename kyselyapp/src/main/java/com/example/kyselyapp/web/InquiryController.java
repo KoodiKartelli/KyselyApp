ecdfdddc6b3a37aa312ffa52a0b855f35a5504be
Hilda Väänänen <hilda.vaananen@gmail.com>
2023-10-30 11:41:20 +0200

    inquiryrepository add ja save tehty

--- kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryController.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryController.java
@@ -1,5 +1,30 @@
 package com.example.kyselyapp.web;
 
-public class InquiryController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.kyselyapp.domain.Inquiry;
+import com.example.kyselyapp.domain.Inquiryrepository;
+
+@Controller
+public class InquiryController {
+    
+    @Autowired
+    Inquiryrepository inquiryRepository;
+
+    @RequestMapping(value = "/addinquiry")
+    public String addInquiry(Model model) {
+         model.addAttribute("inquiry", new Inquiry());
+         return "addinquiry";
+    }
+
+     @RequestMapping(value = "/save", method = RequestMethod.POST)
+     public String save (Inquiry inquiry) {
+        inquiryRepository.save(inquiry);
+        return "redirect:inquirylist";
+     }
+    
 }
