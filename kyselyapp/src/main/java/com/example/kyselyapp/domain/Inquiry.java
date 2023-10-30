ac47dc69a096a5f6cd6ab486903ac85596a658cc
Hilda Väänänen <hilda.vaananen@gmail.com>
2023-10-30 11:20:15 +0200

    inquiry.java luokan muokkaus

--- kyselyapp/src/main/java/com/example/kyselyapp/domain/Inquiry.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/domain/Inquiry.java
@@ -1,10 +1,30 @@
 package com.example.kyselyapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Inquiry {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long inquiryid;
-	String inquiry;
+
+	String title;
 	String description;
 	
+	public Inquiry(String inquiry, String description) {
+		this.title = inquiry;
+		this.description = description;
+	}
+
+	public Inquiry() {
+		this.title = null;
+		this.description = null;
+	}
+
 	public String getDescription() {
 		return description;
 	}
@@ -17,11 +37,18 @@ public class Inquiry {
 	public void setInquiryid(Long inquiryid) {
 		this.inquiryid = inquiryid;
 	}
-	public String getInquiry() {
-		return inquiry;
+	
+	public String getTitle() {
+		return title;
 	}
-	public void setInquiry(String inquiry) {
-		this.inquiry = inquiry;
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Inquiry [inquiryid=" + inquiryid + ", title=" + title + ", description=" + description + "]";
 	}
 	
 }
