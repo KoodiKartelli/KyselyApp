d6ae25e8753f973b14416e18385a77216f2652de
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-10-30 11:42:38 +0200

    InquiryRestController pitäis toimia

--- kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryRestController.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/web/InquiryRestController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +21,7 @@ public class InquiryRestController{
 	
 
 
-@RequestMapping(value="/inquirys", method = RequestMethod.GET)
+@RequestMapping(value="/inquiry", method = RequestMethod.GET)
 public @ResponseBody List<Inquiry> inquiryListRest(){
 	return (List<Inquiry>) repository.findAll();
 }
@@ -28,6 +29,13 @@ public @ResponseBody List<Inquiry> inquiryListRest(){
 @RequestMapping(value="/inquiry/{id}", method = RequestMethod.GET)
 public @ResponseBody Optional<Inquiry> findInquiryRest(@PathVariable("id") Long inquiryid){
 	return repository.findById(inquiryid);
-
 	}
+
+@RequestMapping(value = "/inquirylist")
+public String inquirylist(Model model) {
+	List<Inquiry> inquiry = (List<Inquiry>) repository.findAll();
+	model.addAttribute("inquiry", inquiry);
+	return "inquirylist";
+}
+
 }
