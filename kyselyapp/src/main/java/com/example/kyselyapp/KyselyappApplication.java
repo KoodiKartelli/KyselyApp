e265b6e867418fed98a72c84bd5cb1d81774b2cf
Jenna <143401323+jenvii@user.noreply.github.com>
2023-11-08 12:22:54 +0200

    Uudet kysymykset

--- kyselyapp/src/main/java/com/example/kyselyapp/KyselyappApplication.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/KyselyappApplication.java
@@ -24,16 +24,36 @@ public class KyselyappApplication {
 
 public CommandLineRunner demo(Inquiryrepository inquiryRepository, QuestionRepository questionRepository) {
     return (args) -> {
-        List<Question> questions = new ArrayList<>();
+        List<Question> questions1 = new ArrayList<>();
 
 
-        Inquiry inquiry = new Inquiry("Onko nuoriso pilalla", "No onko", questions);
+        Inquiry inquiry1 = new Inquiry("Haaga-Helian lounaskysely", "Kysely liittyen Haaga-Helian lounaan laatuun", questions1);
 
-		questions.add(new Question("Kysymys 1", inquiry));
-        questions.add(new Question("Kysymys 2", inquiry));
-        questions.add(new Question("Kysymys 3", inquiry));
+		questions1.add(new Question("Kuinka usein käyt kouluruokailussa?", inquiry1));
+        questions1.add(new Question("Onko hintalaatusuhde mielestäsi hyvä?", inquiry1));
+        questions1.add(new Question("Esitä ruokatoiveita tai kerro lempiruokasi.", inquiry1));
 
-        inquiryRepository.save(inquiry);
+        inquiryRepository.save(inquiry1);
+        
+        List<Question> questions2 = new ArrayList<>();
+        
+        Inquiry inquiry2 = new Inquiry("Jouluelokuvakysely", "Kysely parhaimmista jouluelokuvista.", questions2);
+        
+        questions2.add(new Question("Mikä on maailman paras jouluelokuva?", inquiry2));
+        questions2.add(new Question("Mitä elokuvaa et suosittelisi kenellekään?", inquiry2));
+        questions2.add(new Question("Miksi juuri nämä elokuvat ovat parhaimpia?", inquiry2));
+        
+        inquiryRepository.save(inquiry2);
+        
+   List<Question> questions3 = new ArrayList<>();
+        
+        Inquiry inquiry3 = new Inquiry("Kinkkiset kysymykset -kysely", "Maailman ärsyttävimmät kysymykset, joita kaikki vihaa, mutta niitä kysytään silti.", questions3);
+        
+        questions3.add(new Question("Kuuluuko ananas pizzaan?", inquiry3));
+        questions3.add(new Question("Kissat vai koirat?", inquiry3));
+        questions3.add(new Question("Valitse supervoimasi.", inquiry3));
+        
+        inquiryRepository.save(inquiry3);
     };
 
 }
