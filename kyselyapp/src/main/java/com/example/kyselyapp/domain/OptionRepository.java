c1db3f2ea654a8dad03db0f5061d1a70de74d297
Hilda Väänänen <hilda.vaananen@gmail.com>
2023-12-11 11:03:32 +0200

    OptinController luokkaa tehty

--- kyselyapp/src/main/java/com/example/kyselyapp/domain/OptionRepository.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/domain/OptionRepository.java
@@ -1,7 +1,11 @@
 package com.example.kyselyapp.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface OptionRepository extends CrudRepository<Option, Long> {
-    
+
+    List<Option> findByQuestionQuestionId(Long questionId);
+
 }
--- kyselyapp/src/main/java/com/example/kyselyapp/web/OptionController.java
+++ kyselyapp/src/main/java/com/example/kyselyapp/web/OptionController.java
@@ -1,11 +1,52 @@
 package com.example.kyselyapp.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.kyselyapp.domain.Inquiry;
+import com.example.kyselyapp.domain.Option;
+import com.example.kyselyapp.domain.OptionRepository;
+import com.example.kyselyapp.domain.Question;
+import com.example.kyselyapp.domain.QuestionRepository;
+
 public class OptionController {
     
+    @Autowired
+	private QuestionRepository questionRepository;
 
-    //Luo vastausvaihtoehdot kysymykselle
+    @Autowired
+	private OptionRepository optionRepository;
+
+    //Luo vastausvaihtoehdon kysymykselle
+    @RequestMapping(value = "/addoption/{questionId}", method = RequestMethod.GET)
+	public String addOptionForm(@PathVariable("questionId") Long questionId, Model model) {
+	    Question question = questionRepository.findById(questionId).get();
+	    Option newOption = new Option();
+	    newOption.setQuestion(question);
+	    model.addAttribute("option", newOption);
+        return "addoption"; //addquestion.html
+}
     
-    
-    //Palauttaa kysymysken vastausvaihtoehdot
+    //Tallettaa lomakkeelta tulleet vastausvaihtoehdon tiedot tietokantaan
+    @RequestMapping(value="/saveoption/{questionId}", method = RequestMethod.POST)
+	public String saveOption(@PathVariable("questionId") Long questionId, Option option, Model model) {
+    	option.setQuestion(questionRepository.findById(questionId).get());
+    	optionRepository.save(option);
+    	return "redirect:/questionlist/{inquiryid}";
+}
+    //Palauttaa yhden kysymyksen vastausvaihtoehdot
+	@RequestMapping(value="/optionlist/{questionId}", method = RequestMethod.GET)
+	public String showQuestionsByInquiryId(@PathVariable("questionId") Long questionId, Model model){
+		Question question = questionRepository.findById(questionId).get();
+		List<Option> options = optionRepository.findByQuestionQuestionId(questionId);
+		model.addAttribute("options", options);
+		model.addAttribute("question", question);
+		return "optionlist";
+	}
 
 }
